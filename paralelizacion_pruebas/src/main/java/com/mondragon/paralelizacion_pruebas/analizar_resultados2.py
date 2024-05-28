f2d3297c8810d74558caeb94177e863c5917559e
julengabi <julen.gabilondo@alumni.mondragon.edu>
2024-05-28 10:12:58 +0200

    paralelizacion y web

--- paralelizacion_pruebas/src/main/java/com/mondragon/paralelizacion_pruebas/analizar_resultados2.py
+++ paralelizacion_pruebas/src/main/java/com/mondragon/paralelizacion_pruebas/analizar_resultados2.py
@@ -2,25 +2,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-#sin_paralelizar_df = pd.read_csv("C:/Users/julga/Documents/MU/23-24/wanderlust/Web/paralelizacion_pruebas/promedio_tiempos_tipo2.csv")
 con_paralelizar_df = pd.read_csv("C:/Users/julga/Documents/MU/23-24/wanderlust/Web/paralelizacion_pruebas/promedio_tiempos_tipo1.csv")
 
-#sin_paralelizar_df = sin_paralelizar_df.sort_values(by="cantidad_de_datos")
 con_paralelizar_df = con_paralelizar_df.sort_values(by="cantidad_de_datos")
 
 plt.figure(figsize=(14, 7))
-
-plt.subplot(1, 2, 1)
-sns.lineplot(
-    data=sin_paralelizar_df,
-    x="cantidad_de_datos",
-    y="tiempo_en_milisegundos",
-    label="Sin Paralelizar",
-)
-plt.title("Tiempos de ejecución sin paralelizar")
-plt.title("Tiempos de Ejecución Sin Paralelizar")
-plt.xlabel("Cantidad de Datos")
-plt.ylabel("Tiempo en Milisegundos")
 
 # Gráfico para tiempos con paralelización
 plt.subplot(1, 2, 2)
@@ -39,14 +25,6 @@ plt.ylabel("Tiempo en Milisegundos")
 plt.tight_layout()
 plt.show()
 
-# Comparación de ambos métodos
-plt.figure(figsize=(14, 7))
-sns.lineplot(
-    data=sin_paralelizar_df,
-    x="cantidad_de_datos",
-    y="tiempo_en_milisegundos",
-    label="Sin Paralelizar",
-)
 sns.lineplot(
     data=con_paralelizar_df,
     x="cantidad_de_datos",
